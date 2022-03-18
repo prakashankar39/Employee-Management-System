a6af73c40f665f4072715a20e85fce177a9211a9
Prakash Shankar <prakashankar39@gmail.com>
2022-03-18 00:05:13 -0400

    project1 files

---
+++ src/main/java/com/prakash/employeemanagementsystem/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.prakash.employeemanagementsystem.service;
+
+import com.prakash.employeemanagementsystem.model.Employee;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
